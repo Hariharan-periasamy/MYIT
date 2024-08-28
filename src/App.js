0cb1b8a655095719a88a98903b5296b892d423f6
Hari <hariharanp629@gmail.com>
2024-08-28 14:20:23 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Index from "./section2/Index";
+import Footer from "./footer/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Index/>
+      <Footer/>  
+    </div>
+  );
+}
+
+export default App;
